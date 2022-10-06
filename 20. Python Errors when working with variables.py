8b8bdb39cd675fa23e03d3a91d70e47dc74bfc25
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-10-06 22:58:47 +0500

    init: python task

---
+++ 20. Python Errors when working with variables.py
@@ -0,0 +1,17 @@
+'''Найдите в программе необъявленную переменную и объявите ее, присвоив ей значение 'Dragon'.
+
+После выполнения программы результат на экране должен выглядеть так:
+
+Targaryen
+ and
+Dragon'''
+
+family = 'Targaryen'
+
+# BEGIN
+pet = 'Dragon'
+# END
+
+print(family)
+print(' and ')
+print(pet)
