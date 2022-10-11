873fcdf27e31d57e25f322eb2256cb62a4f7f741
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-10-11 23:25:26 +0500

    init: python task

---
+++ 26. Python Interpolation.py
@@ -0,0 +1,7 @@
+'''Выведите на экран строку Do you want to eat, <name>?. Где вместо <name> должна использоваться переменная stark. Вывод должен получиться таким:'''
+
+stark = 'Arya'
+
+# BEGIN
+print(f'Do you want to eat, {stark}?')
+# END
