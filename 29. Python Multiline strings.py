d058d7b03e248c576693691c85336fa086b094fa
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-10-16 00:04:59 +0500

    init: python task

---
+++ 29. Python Multiline strings.py
@@ -0,0 +1,15 @@
+'''Запишите в переменную text текст, который приведен ниже. Используйте тройные кавычки.
+
+Lannister, Targaryen, Baratheon, Stark, Tyrell...
+they're all just spokes on a wheel.
+This one's on top, then that one's on top, and on and on it spins,
+crushing those on the ground.'''
+
+# BEGIN
+text = '''Lannister, Targaryen, Baratheon, Stark, Tyrell...
+they're all just spokes on a wheel.
+This one's on top, then that one's on top, and on and on it spins,
+crushing those on the ground.'''
+# END
+
+print(text)
