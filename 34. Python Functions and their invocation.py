4998cb41d9a80c20861f297b2d414a5c0bf0670b
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-10-22 20:24:29 +0500

    init: python task

---
+++ 34. Python Functions and their invocation.py
@@ -0,0 +1,11 @@
+'''В коде программы определены две переменные, содержащие имена компаний. Посчитайте их общую длину в символах и выведите ее на экран.'''
+
+company1 = 'Apple'
+company2 = 'Samsung'
+
+# BEGIN
+company1_len = len(company1)
+company2_len = len(company2)
+
+print(company1_len + company2_len)
+# END
