c6a0f15621763ce7d8fb4ce766989e243591c6ff
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-10-30 23:53:21 +0500

    init: python task

---
+++ 41. Python Objects.py
@@ -0,0 +1,8 @@
+'''Приведите строку text к нижнему регистру и напечатайте её на экран. Пример метода, выполняющего эту задачу приведен в теории.'''
+
+text = 'a MIND needs Books as a Sword needS a WHETSTONE.'
+
+# BEGIN
+lower_text = text.lower()
+print(lower_text)
+# END
