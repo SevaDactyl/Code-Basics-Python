fceace29dd8306a94741d302b9b80a4a326ff3eb
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-11-10 23:55:50 +0500

    init: python task

---
+++ 46. Python Returning values.py
@@ -0,0 +1,7 @@
+'''Реализуйте функцию say_hurray_three_times(), которая возвращает строку 'hurray! hurray! hurray!'.'''
+
+# BEGIN
+def say_hurray_three_times():
+    word = 'hurray!'
+    return f'{word} {word} {word}'
+# END
