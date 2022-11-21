e87e8391d8cce1c0713fc88845b3c8acfe50659d
Vsevolod Rukavishnikov <107400712+SevaDactyl@users.noreply.github.com>
2022-11-21 23:33:48 +0500

    init: python task

---
+++ 51. Python Predicates.py
@@ -0,0 +1,7 @@
+'''Напишите функцию is_mister(), которая принимает строку и проверяет, является ли она словом 'Mister'.
+
+is_mister('Mister') # True
+is_mister('Missis') # False'''
+
+def is_mister(string):
+    return string == 'Mister'
